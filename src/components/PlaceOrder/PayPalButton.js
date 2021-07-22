8f88be2648cc2b20c4a1ae73939115ea0addbafc
Phú Huỳnh Trần <phu.ht@medofa.com>
2021-07-22 22:27:50 +0700

    fix payment with paypal

--- src/components/PlaceOrder/PayPalButton.js
+++ src/components/PlaceOrder/PayPalButton.js
@@ -29,11 +29,10 @@ export default class PayPalButton extends React.Component {
         }
  
         let env = 'sandbox'; // you can set here to 'production' for production
-        let currency = 'VND'; // or you can set this value from your props or state
+        let currency = 'USD'; // or you can set this value from your props or state
         let total = this.props.total; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
-        
-       
+        total = (total/23000).toFixed(2);
         const client = {
             sandbox:    pp,
             production: 'YOUR-PRODUCTION-APP-ID',
